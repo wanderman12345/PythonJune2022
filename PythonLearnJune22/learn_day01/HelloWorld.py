d5617b6793255d47b6302111cef5a470a9b0f445
Rishi Arun <rishi.h2kinfosys@gmail.com>
2022-06-14 06:51:35 +0530

    Python Day 01

---
+++ PythonLearnJune22/learn_day01/HelloWorld.py
@@ -0,0 +1,17 @@
+# Comment are descriptions of loc - english - single line comment
+# Print hello world in console
+
+
+print("Hello World")
+print('Hello World')
+
+'''
+This is called 
+Multi Line Comment
+You can write big descriptions here
+'''
+
+"""
+This is also allowed as comment? -NO
+This is called as docString - String with multiple lines
+"""
---
+++ PythonLearnJune22/learn_day01/Variables.py
@@ -0,0 +1,11 @@
+age = 21
+percent = 99.45
+complex_var = 5 + 6j
+name = "Niel Armstrong"
+isValid = True
+
+print(age, type(age))
+print(percent, type(percent))
+print(complex_var, type(complex_var))
+print(name, type(name))
+print(isValid, type(isValid))
